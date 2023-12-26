83899bb1406c59f270f47a97e329ae56457c6e42
darkness <122851321+darkness11231@users.noreply.github.com>
2023-12-26 09:54:10 +0700

    Update only-l2tp.sh

--- only-l2tp.sh
+++ only-l2tp.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [[ $(ulimit -c) != "0" ]]; then
-  echo "Im Watching You..."
-  echo "- @user_legend"
-  exit 1
-fi
 
 red='\e[1;31m'
 green='\e[0;32m'
@@ -11,100 +6,6 @@ NC='\e[0m'
 IP=$(curl -sS ipinfo.io/ip > /tmp/ipaddress.txt)
 MYIP=$(cat /tmp/ipaddress.txt)
 
-# cek wget & curl
-if ! which wget > /dev/null; then
-clear
-echo -e "${red}Wah Mau Belajar Nakal Yah !${NC}"
-sleep 2
-exit 0
-clear
-else
-echo "Wget is already installed"
-fi
-
-if ! which curl > /dev/null; then
-clear
-echo -e "${red}Wah Mau Belajar Nakal Yah !${NC}"
-sleep 2
-exit 0
-clear
-else
-echo "curl is already installed"
-fi
-
-fileee=/usr/bin/wget
-minimumsize=400000
-actualsize=$(wc -c <"$fileee")
-if [ $actualsize -ge $minimumsize ]; then
-clear
-echo -e "${green}Checking...${NC}"
-else
-clear
-echo -e "${red}Permission Denied!${NC}";
-echo "Reason : Modified Package To Bypass Sc"
-exit 0
-fi
-
-fileeex=/usr/bin/curl
-minimumsizex=15000
-clear
-actualsizex=$(wc -c <"$fileeex")
-if [ $actualsizex -ge $minimumsizex ]; then
-clear
-echo -e "${green}Checking...${NC}"
-else
-clear
-echo -e "${red}Permission Denied!${NC}";
-echo "Reason : Modified Package To Bypass Sc"
-exit 0
-fi
-
-
-# data server
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-cekcloudflare=$(curl -sS http://ip-api.com/json | jq .as | grep -o "Cloudflare")
-if [[ "$cekcloudflare" = "Cloudflare" ]]; then
-cekdomen=$(cat /etc/xray/domain)
-MYIP=$(dig +short "$cekdomen" | head -n 1)
-fi
-repogithub='darkness11231/premium/main'
-exit 1
-else
-echo -e "${green}Script Active !${NC}"
-clear
-fi
-done
-
-# cek ip address
-checkipaddres=( `grep -E "^### $data" "/tmp/logs.txt" | awk '{print $4}' | sort | uniq` )
-if [[ "$MYIP" = "$checkipaddres" ]]; then
-echo -e "${green}IP Address Accepted${NC}"
-clear
-else
-echo -e "${red}IP Address Not Found In Our Database${NC}"
-echo -e "Contact Admin : t.me/user_legend"
-rm -rf /tmp/logs.txt
-rm -rf /tmp/ipaddress.txt
-exit 1
-fi
-
-# cek client name
-clientname=$(cat /usr/local/etc/clientname)
-checkclient=( `grep -E "^### $data" "/tmp/logs.txt" | awk '{print $2}' | sort | uniq` )
-if [[ "$clientname" = "$checkclient" ]]; then
-echo -e "${green}Client Name Accepted${NC}"
-clear
-else
-echo -e "${red}Client Name Not Compatible !${NC}"
-echo -e "Contact Admin : t.me/user_legend"
-rm -rf /tmp/logs.txt
-rm -rf /tmp/ipaddress.txt
-exit 1
-fi
-rm -rf /tmp/logs.txt
-rm -rf /tmp/ipaddress.txt
-clear
 
 # Define variables
 DEBIAN_FRONTEND=noninteractive apt-get -y install iptables-persistent strongswan strongswan-pki libcharon-extra-plugins net-tools wget certbot charon-systemd
